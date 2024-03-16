4e59a5dbeb348a098b9e877ffd90d256be14fded
code406 <davidpm99@gmail.com>
2024-03-16 02:36:24 +0100

    feat: add vercel & astro DB. create model

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [db()],
+  output: "server",
+  adapter: vercel()
+});
